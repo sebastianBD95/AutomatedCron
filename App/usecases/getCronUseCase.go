e1eac5d3f7d9005c3daa5c45e3c7538a82ae8fa2
Sebastían Benítez Díaz <sebastianbd95@gmail.com>
2021-05-06 22:16:07 -0500

    get handler done

---
+++ App/usecases/getCronUseCase.go
@@ -0,0 +1,42 @@
+package usecases
+
+import (
+	"context"
+	"time"
+
+	"github.com/reactivex/rxgo/v2"
+	"github.com/sebastianBD95/AutomatedCron/App/infrastructure"
+	"github.com/sebastianBD95/AutomatedCron/App/usecases/model"
+	"github.com/sirupsen/logrus"
+)
+
+func GetCronUsesCase() (model.CronAutomated, error) {
+
+	logrus.Info("Creating  channel")
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	var c model.CronAutomated
+	ch := make(chan rxgo.Item, 10)
+
+	observable := rxgo.FromChannel(ch, rxgo.WithPublishStrategy()).
+		Map(infrastructure.SaveCron)
+
+	observable.Connect(ctx)
+	ch <- rxgo.Of(c)
+	close(ch)
+
+	var cr model.CronAutomated
+	var err error
+	for items := range observable.Observe() {
+
+		if items.Error() {
+			err = items.E
+		} else {
+			cr = items.V.(model.CronAutomated)
+		}
+
+	}
+	logrus.Info("Cron getted with Id: " + cr.ID)
+	return cr, err
+}
--- App/usecases/model/cron.go
+++ App/usecases/model/cron.go
@@ -1,9 +1,9 @@
 package model
 
 type CronAutomated struct {
-	ID     string            `json: "id"`
-	Name   string            `Json: "name"`
-	Url    string            `Json: "url"`
-	Header map[string]string `json: "header"`
-	Result string            `json: result`
+	ID     string            `schema: "id"`
+	Name   string            `schema: "name"`
+	Url    string            `schema: "url"`
+	Header map[string]string `schema: "header"`
+	Result string            `schema: "result"`
 }
