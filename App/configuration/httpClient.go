7689f6c768371e5a5bfee5821308872efa711fdc
Sebastían Benítez Díaz <sebastianbd95@gmail.com>
2021-05-18 21:50:21 -0500

    solving errors

--- App/configuration/httpClient.go
+++ App/configuration/httpClient.go
@@ -1,28 +1,20 @@
 package configuration
 
-import 
+import "fmt"
 
-
-func ProccessRequest( id,name,verb,url,body,headers,err string){
-
+func ProccessRequest(id, name, verb, url, body, headers, err string) {
 
 }
 
+func ResolveRequest(url, body, headers, verb string) {
 
-func ResolveRequest(url,body,headers,verb string) (
-	switch verb {
-		case model.httpVerb.GET:
-			 doGet(url,headers)
-		case model.httpVerb.POST:
-			 doGet(url,headers)
-		case model.httpVerb.PUT:
-			 doGet(url,headers)
-		case model.httpVerb.DELETE:
-			 doGet(url,headers)	
-		}
+	switch {
+	case verb == "GET":
+		doGet(url, headers)
+	}
 }
 
-
-func doGet(url,headers){
-
+//do a Get Request
+func doGet(url, headers) {
+	fmt.Println("Doing GET")
 }
--- App/usecases/model/cron.go
+++ App/usecases/model/cron.go
@@ -1,9 +1,14 @@
 package model
 
+import (
+	"github.com/mileusna/crontab"
+)
+
 type CronAutomated struct {
-	ID     string            `schema: "id"`
-	Name   string            `schema: "name"`
-	Url    string            `schema: "url"`
-	Header map[string]string `schema: "header"`
-	Date   string            `schema: "result"`
+	ID     string            `schema:"id"`
+	Name   string            `schema:"name"`
+	Url    string            `schema:"url"`
+	Header map[string]string `schema:"header"`
+	Date   string            `schema:"result"`
+	Cron   crontab.Crontab   `schema:"-"`
 }
